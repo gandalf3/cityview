9c514d8f24a338494f952caafbab9bb059f14c38
gandalf3 <gandalf3@blendermonkey.com>
2017-04-08 18:19:19 -0700

    add popups to neighbor polys

--- web/html/js/start.js
+++ web/html/js/start.js
@@ -6,17 +6,26 @@ var map = L.map('map').setView([44.0539, -123.0944], 12);
 L.tileLayer("http://a.tile.stamen.com/terrain/{z}/{x}/{y}.png", { maxzoom : 18 }).addTo(map)
 
 
+function popUp(f, l){
+ 	l.bindPopup(f.properties);
+	
+	l.addEventListener('mouseover', function (e) {
+	        this.openPopup();
+     	});
+	
+
+        l.addEventListener('mouseout', function (e) {
+                this.closePopup()
+	});
+	
+}
 // add neighborhoods
 var neighborStyle = {
 	"weight": 1,
 }
-
-var neighborPolys = new L.GeoJSON.AJAX("data/neighborhoods_geo.json", {
-	style: neighborStyle
+var neighborPolys = new L.GeoJSON.AJAX("data/neighborhoods_geo.json",{
+	onEachFeature:popUp,
+	style:neighborStyle,
 });
-
-var ugbPoly = new L.GeoJSON.AJAX("data/ugb_geo.json", {
-	style: neighborStyle
-});
-//ugbPoly.addTo(map);
 neighborPolys.addTo(map);
+
